d549867beaf14f3dd6296933dad290524f7fe0d6
WooRiGyeoRe <rufp0607@naver.com>
2024-07-08 16:41:56 +0900

    Initial commit

---
+++ lib/riverpod.dart
@@ -0,0 +1,14 @@
+
+
+/*
+
+  ★ Flutter의 RiverPod이란?  => 상태 관리 라이브러리 중 하나로 Provider와 사용성은 같지만, 그 문제점을 개선한 버전!
+
+  (플러터 RiverPod에 대해 이해하기 위해서는 기본적으로 Provider에 대한 이해가 선행되어야 함.)
+
+  일반적인 Provider는 값을 읽을 수는 있지만, 수정할 순 없음! 
+  값을 읽고 수정하려면 상태를 변경할 수 있는 Provider인 StateProvider를 사용해야 함. 
+  일부 로직을 같이 사용하는 경우에는 StateNotifierProvider를 사용함. 
+
+
+*/
