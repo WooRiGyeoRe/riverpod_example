6a3366f5860953c0e78563a882dce93acd3b29c1
WooRiGyeoRe <rufp0607@naver.com>
2024-07-11 11:38:23 +0900

    플러터 다운 그레이드했더니 package identifier or launch activity not found...(생략) 문제가 생겨서 다시 플러터 업그레이드 하고, flutter riverpod snippets 확장팩 사용하는 걸로.....

--- lib/riverpod/state_provider.dart
+++ lib/riverpod/state_provider.dart
@@ -4,11 +4,3 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // 0은 내가 관리할 거라 <int>로 넣어주면 됨
 final numberProvider = StateProvider<int>((ref) => 0);
-
-final a = Provider((ref) => null);
-
-final t = FutureProvider((ref) => null);
-
-final testProvider = FutureProvider((ref) async {
-  return;
-});
